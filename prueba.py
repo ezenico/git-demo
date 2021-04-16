d6de34e8d9eb20f15f5a42350d260a7801c50d76
Ezequiel Farina <ezekpo1995@gmail.com>
2021-04-16 12:28:44 +0200

    Añado un fichero de prueba en python

---
+++ prueba.py
@@ -0,0 +1,7 @@
+print("Hola mundo")
+
+# Basic function adition 
+x = int(input("Please enter the first number: "))
+y = int(input("Please enter the second numeber: "))
+
+print("The adition result is: ", x+y)
